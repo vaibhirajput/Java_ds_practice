ff284282948b38e8533effb1cca186f2b0ba7360
Vaibhav Rajput <truemusclescience@gmail.com>
2022-03-29 13:49:53 +0530

    ..

--- src/generic.java
+++ src/generic.java
@@ -2,7 +2,7 @@ public class generic {
     public static void main(String[] args) {
         generic gn = new generic();
         Integer [] b1 = { 45,56,23,89,21,23,56};
-        String [] b2 = {"pooja", "vaibhav", "rohit", "kohli"};
+        String [] b2 = { "vaibhav", "rohit", "kohli"};
         gn.genericc(b1);
         System.out.println();
         gn.genericc(b2);
